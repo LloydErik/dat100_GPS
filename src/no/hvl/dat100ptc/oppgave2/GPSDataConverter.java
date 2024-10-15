10a8055efffe6a667a275967505b35a95446cac6
ErikBekkemellem <erik.bekkemellem@gmail.com>
2024-10-15 11:56:36 +0200

    gjorde oppgave 1 a

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -7,23 +7,34 @@ public class GPSDataConverter {
 
 	
 	private static int TIME_STARTINDEX = 11; 
-
+	
+	
 	public static int toSeconds(String timestr) {
 		
 		int secs;
 		int hr, min, sec;
 		
-		// TODO
-		throw new UnsupportedOperationException(TODO.method());
+		hr = 8; 
+		min = 52; 
+		sec = 26;
+		
+		secs = hr *60*60 + min*60 + sec;
+
+		return secs;
 		
 	}
 
 	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
 
-		GPSPoint gpspoint;
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
+		int time = 8*60*60 + 52*60 + 26;
+		double latitude = 60.385390;
+		double longitude = 5.217217;
+		double elevation = 61.9;
+		
+		
+		GPSPoint gpspoint = new GPSPoint(time, latitude, longitude, elevation);
+		
+		return gpspoint;
 		
 	}
 	
